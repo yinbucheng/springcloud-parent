b4e52cb12b947cd1260f6d0c59164520118e6f55
yinbucheng <yin.chong@intellif.com>
2019-03-30 22:04:28 +0800

    添加zuul中token校验

--- readme.txt
+++ readme.txt
@@ -1,1 +1,2 @@
 #这里是最简单的分布式项目其中包括 eureka,fegin,zipkin,zuul
+#添加zuul拦截逻辑
---
+++ server-zuul/src/main/java/cn/intellif/serverzuul/config/MyFilter.java
@@ -0,0 +1,45 @@
+package cn.intellif.serverzuul.config;
+
+import com.ctrip.framework.apollo.core.utils.StringUtils;
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+public class MyFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 10;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        String token = request.getParameter("token");
+        if(StringUtils.isBlank(token)){
+            ctx.setSendZuulResponse(false); //不对其进行路由
+            ctx.setResponseStatusCode(400);
+            ctx.setResponseBody("token is empty");
+            ctx.set("isSuccess", false);
+        }else{
+            ctx.setSendZuulResponse(true); //对请求进行路由
+            ctx.setResponseStatusCode(200);
+            ctx.set("isSuccess", true);
+        }
+        return null;
+    }
+}
