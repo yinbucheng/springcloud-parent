013ded02d113b8f6c5a74bc215d211f5450fc39a
yinchong <yin.chong@intellif.com>
2019-04-02 08:55:11 +0800

    添加eureka的权限验证

--- readme.txt
+++ readme.txt
@@ -7,6 +7,7 @@ static class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable();
+		super.configure(http);
 	}
 }
 
--- register-center/src/main/java/cn/intellif/register/center/RegisterCenterApplication.java
+++ register-center/src/main/java/cn/intellif/register/center/RegisterCenterApplication.java
@@ -21,6 +21,7 @@ public class RegisterCenterApplication {
         @Override
         protected void configure(HttpSecurity http) throws Exception {
             http.csrf().disable();
+            super.configure(http);
         }
     }
 }
