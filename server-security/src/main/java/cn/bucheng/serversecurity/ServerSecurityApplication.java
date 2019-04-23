8dc79f426864253956267c7548ef81152b1477c6
yinchong <yin.chong@intellif.com>
2019-04-23 19:29:47 +0800

    添加权限模块

---
+++ server-security/src/main/java/cn/bucheng/serversecurity/ServerSecurityApplication.java
@@ -0,0 +1,13 @@
+package cn.bucheng.serversecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerSecurityApplication.class, args);
+    }
+
+}
