f359b3708497864ed5e8ab485bebc9544d20fbaf
yinchong <yin.chong@intellif.com>
2019-04-23 20:28:17 +0800

    自定义realm等待实现

---
+++ server-security/src/test/java/cn/bucheng/serversecurity/customream/CustomRealm.java
@@ -0,0 +1,25 @@
+package cn.bucheng.serversecurity.customream;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+
+/**
+ * @ClassName CustomRealm
+ * @Author buchengyin
+ * @Date 2019/4/23 20:24
+ **/
+public class CustomRealm extends AuthorizingRealm {
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+        return null;
+    }
+
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        return null;
+    }
+}
