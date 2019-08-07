1c1db55af5c043277087730919f30df26035a765
yinchong <yin.chong@intellif.com>
2019-08-07 08:48:48 +0800

    优化tomcat连接

---
+++ server-provider/src/main/java/cn/intellif/server/provider/config/WebServerConfiguration.java
@@ -0,0 +1,26 @@
+package cn.intellif.server.provider.config;
+
+import org.apache.catalina.connector.Connector;
+import org.apache.coyote.http11.Http11NioProtocol;
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.stereotype.Component;
+
+@Component
+public class WebServerConfiguration implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
+    @Override
+    public void customize(ConfigurableWebServerFactory factory) {
+        ((TomcatServletWebServerFactory)factory).addConnectorCustomizers(new TomcatConnectorCustomizer() {
+            @Override
+            public void customize(Connector connector) {
+                Http11NioProtocol protocol = (Http11NioProtocol)connector.getProtocolHandler();
+                //定制keepalivetimeout,设置30秒内没有请求则服务自动断开keepalive连接
+                protocol.setKeepAliveTimeout(30000);
+                //当客户端发送超过10000个请求则自动断开keepalive链接
+                protocol.setMaxKeepAliveRequests(10000);
+            }
+        });
+    }
+}
